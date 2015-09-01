3bbc6375d6a6cf8eebbc67c0cafc46dd46bb8546
Steven-Kha <stevenvkha@gmail.com>
2015-09-01 09:52:14 -0700

    Testing

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 TODO(your name) <TODO(your email)>
+ * Copyright &copy; 2015 Steven Kha <Stevenvkha@gmail.com>
  * Released under the [MIT License] (http://opensource.org/licenses/MIT)
  * ------------------------------------------------------------------------- */
 
