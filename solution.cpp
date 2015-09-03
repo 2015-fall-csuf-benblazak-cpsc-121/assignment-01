bbe448ccfdc80b301cf7a21f13f76c18558f3552
George Calderon <gcald@csu.fullerton.edu>
2015-09-02 23:28:46 -0700

    Assignment-01
    George Calderon
    Convert integers to Roman numerals

--- solution.cpp
+++ solution.cpp
@@ -1,23 +1,120 @@
 /* ----------------------------------------------------------------------------
- * Copyright &copy; 2015 TODO(your name) <TODO(your email)>
- * Released under the [MIT License] (http://opensource.org/licenses/MIT)
- * ------------------------------------------------------------------------- */
+* Copyright &copy; 2015 George Calderon <gcald@csu.fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
 
 /**
- * TODO: program description
- */
+* This program converts integers into Roman numerals.
+*/
 
 // TODO: `#include`s and function prototypes
 
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
 // ----------------------------------------------------------------------------
+string ConRom(int a);
+string again, roman;
 
 int main() {
-    // TODO
-
-    return 0;  // success
+	// TODO
+	while (again != "n" && again != "N")
+	{
+		int a;
+		cout << "Please enter an integer:" << endl;
+		cin >> a;
+		if (a >= 4000 || a <= 0)
+		{
+			cout << "Integer must be from 1 - 3999." << endl;
+		}
+		else
+		{
+			cout << "Roman Numeral: " << ConRom(a) << endl;
+		}
+		cout << endl << "Would you like to convert another integer? (y/n)" << endl << endl;
+		cin >> again;
+	}
+	//system("PAUSE");
+	return 0;  // success
 }
 
 // ----------------------------------------------------------------------------
 
 // TODO: other functions
+string ConRom(int a)
+{
+	roman = "";
+	if (a >= 1000)
+	{
+		for (int i = 0; i < a / 1000; i++)
+		{
+			roman += "M";
+		}
+		a %= 1000;
+	}
 
+	if (a >= 100)
+	{
+		if (a / 100 == 9)
+			roman += "CM";
+		else if (a / 100 >= 5)
+		{
+			roman += "D";
+			for (int i = 0; i < a / 100 - 5; i++)
+				roman += "C";
+		}
+		else if (a / 100 == 4)
+			roman += "CD";
+		else
+		{
+			for (int i = 0; i < a / 100; i++)
+				roman += "C";
+		}
+		a = a % 100;
+		cout << a << endl;
+	}
+	
+	if (a >= 10)
+	{
+		if (a / 10 == 9)
+			roman += "XC";
+		else if (a / 10 >= 5)
+		{
+			roman += "L";
+			for (int i = 0; i < a / 10 - 5; i++)
+				roman += "X";
+		}
+		else if (a / 10 == 4)
+			roman += "XL";
+		else
+		{
+			for (int i = 0; i < a / 10; i++)
+				roman += "X";
+		}
+		a = a % 10;
+	}
+
+	if (a >= 1)
+	{
+		if (a == 9)
+			roman += "IX";
+		else if (a >= 5)
+		{
+			roman += "V";
+			for (int i = 0; i < a - 5; i++)
+				roman += "I";
+		}
+		else if (a == 4)
+			roman += "IV";
+		else
+		{
+			for (int i = 0; i < a; i++)
+				roman += "I";
+		}
+	}
+	return roman;
+}
