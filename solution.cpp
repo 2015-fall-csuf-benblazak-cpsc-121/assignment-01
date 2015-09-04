f73d1d1081dc88d7869af66add76277a1474a785
amyaranda <amyaranda013@gmail.com>
2015-09-03 23:00:54 -0700

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -13,6 +13,127 @@
 
 int main() {
     // TODO
+	#include <iostream>
+#include<string>
+ 
+;
+ 
+int main() {
+ 
+string romanNumeral;
+int integer;
+int number;
+ 
+cout << "Please type in an integer:" ;
+cin >> integer;
+ 
+if ((integer >= 4000) || (integer <=0)) {
+cout <<endl << "Does not exist" << endl;
+}
+ 
+else{
+ 
+if (integer >= 1000) {
+piece = (integer / 1000);
+ 
+for (int i=0; i <number; i++){
+romanNumeral +=  'M';
+}
+integer %=1000;
+}
+ 
+if (integer >= 100 {
+piece = (integer / 100);
+}
+ 
+if (number == 9) {
+romanNumeral += 'CM';
+}
+ 
+else if (number >=5){
+romanNumeral += 'D';
+ 
+for (int i=0; i < number -5, i++) {
+romanNumeral =+ 'C';
+}
+}
+else if (number ==4){
+romanNumeral += 'CD';
+}
+else if (number >= 1) {
+for (int i=0; i <number; i++){
+romanNumeral += 'C';
+}
+}
+integer %100;
+ 
+}
+if (integer >= 10) {
+number = (integer / 10);
+ 
+if (number ==9) {
+romanNumeral += 'XC';
+ 
+}
+ 
+else if (number >= 5) {
+romanNumeral +=  'L';
+ 
+for (int i =0; i < number -5; i++) {
+romanNumeral += 'X'
+}
+}
+ 
+else if (number ==4){
+romanNumeral += 'XL';
+}
+ 
+else if ( number >= 1) {
+ 
+for (int i=0; i< number; i++){
+romanNumeral += 'X';
+}
+}
+integer %10;
+}
+ 
+if (integer >= 1) {
+piece = integer;
+ 
+if (number == 9) {
+romanNumeral +=  'IX';
+}
+ 
+else if (number >=5) {
+romanNumeral += 'V';
+ 
+for (int i=0; i < number- 5 ; i++) {
+romanNumeral += 'I';
+}
+ 
+}
+ 
+else if (number ==4) {
+romanNumeral += 'IV';
+}
+else if (number >= 1) {
+ 
+for (int i=0; i < number; i++) {
+romanNumeral += 'I';
+}
+}
+}
+cout << “Roman Numeral:"  << roman << endl;
+}
+ 
+ 
+ 
+return 0;
+}
+ 
+	}
+}
+
 
     return 0;  // success
 }
